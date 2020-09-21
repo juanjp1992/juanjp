4197f4ff3759574978d3c6a375a518aee6ad0922
juanjp1992 <56151844+juanjp1992@users.noreply.github.com>
2020-09-21 11:25:00 +0200

    Add files via upload

---
+++ Generador_numeros_aleatorios.java
@@ -0,0 +1,53 @@
+/*
+ * Decompiled with CFR 0.0.
+ * 
+ * Could not load the following classes:
+ *  java.io.PrintStream
+ *  java.lang.Math
+ *  java.lang.Object
+ *  java.lang.String
+ *  java.lang.System
+ */
+import java.io.PrintStream;
+
+public class Generador_numeros_aleatorios {
+    public static void main(String[] args) {
+        int[] n = new int[36];
+        for (int i = 0; i < n.length; ++i) {
+            int numero_random = (int)(Math.random() * 100.0);
+            if (numero_random == n[i]) continue;
+            n[i] = numero_random;
+        }
+        int ancho_matriz = n.length - 1;
+        for (int j = 0; j <= n.length - 1; ++j) {
+            int numero_random = (int)(Math.random() * 100.0);
+            System.out.println("J =" + j + " ANCHO MATRIZ = " + ancho_matriz);
+            if (n[j] == n[ancho_matriz] && j != ancho_matriz) {
+                System.out.println("NUMERO REPETIDO, n[" + j + "] = n[" + ancho_matriz + "]");
+                System.out.print("CAMBIADO = " + n[j] + " por ");
+                n[j] = numero_random;
+                System.out.print(String.valueOf((int)n[j]) + "\n");
+                if (n[j] == n[ancho_matriz] && j != ancho_matriz) {
+                    System.out.println("coincide as\u00ed que resto 1.");
+                    --j;
+                } else if (j == n.length - 1) {
+                    System.out.println("Cambio");
+                    j = 0;
+                    --ancho_matriz;
+                }
+            }
+            if (j != n.length - 1) continue;
+            System.out.println("Cambio externo");
+            if (j == n.length - 1 && ancho_matriz == 0) {
+                j = n.length + 1;
+                continue;
+            }
+            j = 0;
+            --ancho_matriz;
+        }
+        for (int z = 0; z < n.length; ++z) {
+            System.out.println("VARIABLE n[" + z + "] = " + n[z]);
+        }
+    }
+}
+
